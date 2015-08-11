0c2fc8d6c26e66f39fde401ca3fdbfcf93b91d9d
Kyle Wilson <kwilson@ooyala.com>
2015-08-11 15:27:57 -0700

    fixed rsync command format

--- sync-internal-routers.sh
+++ sync-internal-routers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rsync -e -av root@ec2-54-90-190-117.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs
+rsync -av -e ssh root@ec2-54-90-190-117.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs
 /router-internal1
 
-rsync -e -av root@ec2-54-205-175-47.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal2
+rsync -av -e ssh root@ec2-54-205-175-47.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal2
 
-rsync -e -av root@ec2-54-90-236-118.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal3
+rsync -av -e ssh root@ec2-54-90-236-118.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal3
--- sync-manager.sh
+++ sync-manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -e -av root@ec2-54-82-201-53.compute-1.amazonaws.com:/var/log/atlantis/manager /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/manager
+rsync -av -e ssh root@ec2-54-82-201-53.compute-1.amazonaws.com:/var/log/atlantis/manager /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/manager
 
