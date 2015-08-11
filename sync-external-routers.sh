7b4eda95f22c41c711c62386dc2a5e36bc612c93
Kyle Wilson <kwilson@ooyala.com>
2015-08-11 15:42:47 -0700

    changed rsync to copy content not dir

--- sync-external-routers.sh
+++ sync-external-routers.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-rsync -av -e ssh root@ec2-54-242-150-47.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs
-/router-external1
+rsync -av -e ssh root@ec2-54-242-150-47.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-external1/
 
-rsync -av -e ssh root@ec2-54-80-138-212.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs
-/router-external2
+rsync -av -e ssh root@ec2-54-80-138-212.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-external2/
 
-rsync -av -e ssh root@ec2-54-91-249-146.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs
-/router-internal3
+rsync -av -e ssh root@ec2-54-91-249-146.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal3/
--- sync-manager.sh
+++ sync-manager.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-rsync -av -e ssh root@ec2-54-82-201-53.compute-1.amazonaws.com:/var/log/atlantis/manager /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/manager
-
+rsync -av -e ssh root@ec2-54-82-201-53.compute-1.amazonaws.com:/var/log/atlantis/manager /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/manager/
