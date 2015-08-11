3d5f0cc8b2cdb0e5130b69d11712f583fbc4b88a
Kyle Wilson <kwilson@ooyala.com>
2015-08-11 15:47:13 -0700

    fixed rsync copy dir instead of contents

--- sync-internal-routers.sh
+++ sync-internal-routers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -av -e ssh root@ec2-54-90-190-117.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal1/
+rsync -av -e ssh root@ec2-54-90-190-117.compute-1.amazonaws.com:/var/log/atlantis/router/ /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal1/
 
-rsync -av -e ssh root@ec2-54-205-175-47.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal2/
+rsync -av -e ssh root@ec2-54-205-175-47.compute-1.amazonaws.com:/var/log/atlantis/router/ /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal2/
 
-rsync -av -e ssh root@ec2-54-90-236-118.compute-1.amazonaws.com:/var/log/atlantis/router /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal3/
+rsync -av -e ssh root@ec2-54-90-236-118.compute-1.amazonaws.com:/var/log/atlantis/router/ /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/router-internal3/
--- sync-supervisor-east-1d.sh
+++ sync-supervisor-east-1d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -av -e ssh root@ec2-54-221-194-18.compute-1.amazonaws.com:/var/log/atlantis/supervisor /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/supervisor-east1d-1/
+rsync -av -e ssh root@ec2-54-221-194-18.compute-1.amazonaws.com:/var/log/atlantis/supervisor/ /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/supervisor-east1d-1/
 
-rsync -av -e ssh root@ec2-23-23-93-12.compute-1.amazonaws.com:/var/log/atlantis/supervisor /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/supervisor-east1d-2/
+rsync -av -e ssh root@ec2-23-23-93-12.compute-1.amazonaws.com:/var/log/atlantis/supervisor/ /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/supervisor-east1d-2/
 
-rsync -av -e ssh root@ec2-54-197-244-32.compute-1.amazonaws.com:/var/log/atlantis/supervisor /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/supervisor-east1d-3/
+rsync -av -e ssh root@ec2-54-197-244-32.compute-1.amazonaws.com:/var/log/atlantis/supervisor/ /Users/kwilson/work/ooyala/ELK/chron-job-copy/logs/supervisor-east1d-3/
